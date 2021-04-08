fe434b93e16603818536bc13cb182afdb4e100a4
Neha Kothari <neha.kothari05@gmail.com>
2021-04-08 19:38:52 +0530

    changes in Order and User bean

--- src/main/java/com/iiitb/customebook/bean/Order.java
+++ src/main/java/com/iiitb/customebook/bean/Order.java
@@ -10,14 +10,27 @@ public class Order implements Cloneable {
     private Integer orderId;
 
     @Column(nullable = false)
-    private Boolean Status;
+    private Boolean orderStatus;
 
-    @Column(nullable = true)
-    private double price;
+    @Column(nullable = false)
+    private String customEBookName;
 
     @ManyToOne
     @JoinColumn(name = "userId", nullable = false)
     private User user;
+
+    @Column(nullable = true)
+    private double totalPrice;
+
+    public Order() {
+    }
+
+    public Order(Boolean orderStatus, String customEBookName, User user, double totalPrice) {
+        this.orderStatus = orderStatus;
+        this.customEBookName = customEBookName;
+        this.user = user;
+        this.totalPrice = totalPrice;
+    }
 
     public Integer getOrderId() {
         return orderId;
@@ -27,29 +40,35 @@ public class Order implements Cloneable {
         this.orderId = orderId;
     }
 
-    public Boolean getStatus() {
-        return Status;
+    public Boolean getOrderStatus() {
+        return orderStatus;
     }
 
-    public void setStatus(Boolean status) {
-        Status = status;
+    public void setOrderStatus(Boolean orderStatus) {
+        this.orderStatus = orderStatus;
     }
 
-    public double getPrice() {
-        return price;
+    public String getCustomEBookName() {
+        return customEBookName;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setCustomEBookName(String customEBookName) {
+        this.customEBookName = customEBookName;
     }
 
-    public  Order(){
-
+    public User getUser() {
+        return user;
     }
 
-    public Order(Integer orderId, Boolean status, double price) { //gotta add order id
-        this.orderId = orderId;
-        Status = status;
-        this.price = price;
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
     }
 }
--- src/main/java/com/iiitb/customebook/bean/User.java
+++ src/main/java/com/iiitb/customebook/bean/User.java
@@ -2,37 +2,40 @@ package com.iiitb.customebook.bean;
 import javax.persistence.*;
 
 @Entity
+@Table
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
 
-    @Column(length=50,nullable = true)
+    @Column
     private String firstName;
 
-    @Column(length=50,nullable = true)
+    @Column
     private String lastName;
 
-    @Column(nullable = false,unique = true)
-    private String emailId;
+    @Column(length=50,nullable = false,unique = true)
+    private String email;
+
+    @Column(length=10)
+    private Integer contactNumber;
 
     @Column(length=50,nullable = false)
     private String password;
 
-    @Column(nullable = false)
-    private String contactNumber;
+    @Column
+    private Boolean isPublisherFlag; //0-> user   1->Publisher
 
-    @Column(nullable = false)
-    private String address;
+    @Column()
+    private String companyName;
 
-    @Column(nullable = false)
-    private boolean isPublisher;
+    public String getCompanyName() {
+        return companyName;
+    }
 
-    @Column(nullable = true)
-    private String CompanyName;
-
-
-    public User() {
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
     }
 
     public int getUserId() {
@@ -43,7 +46,37 @@ public class User {
         this.userId = userId;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(Integer contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 
     public String getPassword() {
         return password;
@@ -53,7 +86,25 @@ public class User {
         this.password = password;
     }
 
+    public Boolean getPublisherFlag() {
+        return isPublisherFlag;
+    }
 
+    public void setPublisherFlag(Boolean publisherFlag) {
+        isPublisherFlag = publisherFlag;
+    }
 
+    public User() {
+    }
 
+    public User(String firstName, String lastName, String email, Integer contactNumber, String password, Boolean isPublisherFlag, String companyName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.contactNumber = contactNumber;
+        this.password = password;
+        this.isPublisherFlag = isPublisherFlag;
+        this.companyName=companyName;
+
+    }
 }
