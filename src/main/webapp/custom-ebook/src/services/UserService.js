cb62e2091bdecdf2d8640d1da5a63fe737cfc97a
Neha Kothari <neha.kothari05@gmail.com>
2021-04-08 19:53:53 +0530

    adding services/UserService.js

---
+++ src/main/webapp/custom-ebook/src/services/UserService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const USER_API_BASE_URL="http://localhost:8081/api/user"
+class UserService{
+
+    getUser()
+    {
+        return axios.get(USER_API_BASE_URL);   //get the data from the API mentioned
+    }
+
+    createUser(user)
+    {
+        return axios.post(USER_API_BASE_URL,user);
+
+    }
+}
+export default new UserService()   //exporting the object of this class
