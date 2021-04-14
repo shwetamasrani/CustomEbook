a0e5a21b62713bfec1f0401f0454f8dc5522aa94
Neha Kothari <neha.kothari05@gmail.com>
2021-04-14 21:55:52 +0530

    changes in BookRepository

--- src/main/java/com/iiitb/customebook/repository/BookRepository.java
+++ src/main/java/com/iiitb/customebook/repository/BookRepository.java
@@ -5,19 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
-import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book,Integer> {
 
 
-    @Query("SELECT b FROM Book b WHERE b.book_name = 'book_name'")
+    @Query("SELECT b FROM Book b WHERE b.book_name like '%book_name%'")
     public Book findByBookName(@Param("book_name") String book_name);
 
-    @Query("SELECT b FROM Book b WHERE b.author = 'author'")
+    @Query("SELECT b FROM Book b WHERE b.author like '%author%'")
     public List<Book> findByAuthor(@Param("author") String author);
 
-    @Query("SELECT b FROM Book b WHERE b.publisher = 'publisher'")
+    @Query("SELECT b FROM Book b WHERE b.publisher like '%publisher%'")
     public List<Book> findByPublisher(@Param("publisher") String publisher);
+
+    @Query("Select b FROM Book b where b.isbnNumber = 'isbnNumber'")
+    public List<Book> findByIsbnNumber(@Param("isbnNumber") String isbnNumber);
 
 
 }
