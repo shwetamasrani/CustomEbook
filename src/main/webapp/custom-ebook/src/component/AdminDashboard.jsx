7eb5a7fe63845c4c11bc245f35ce65ecd19eddaf
lumos-ap <ankita008paul@gmail.com>
2021-04-22 07:20:28 +0530

    admin dashboard changes.

--- src/main/webapp/custom-ebook/src/component/AdminDashboard.jsx
+++ src/main/webapp/custom-ebook/src/component/AdminDashboard.jsx
@@ -1,101 +1,170 @@
-import React,{Component} from "react"
+import React, {Component} from "react"
 import productsInfo from "./Data/productInfo";
 import axios from 'axios';
 import Dashboard from "./Dashboard";
 
-class AdminDashboard extends Component{
-      
-        state = {
-     
-          // Initially, no file is selected
-          selectedFile: null
-         
-        };
-        
-        // On file select (from the pop up)
-        onFileChange = event => {
-        
-          // Update the state
-          this.setState({ selectedFile: event.target.files[0] });
-        
-        };
-        
-        // On file upload (click the upload button)
-        onFileUpload = () => {
-        
-          // Create an object of formData
-          const formData = new FormData();
-        
-          // Update the formData object
-          formData.append(
+class AdminDashboard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            bookName: "",
+            isbnNumber: "",
+            authorName: "",
+            publisher: "",
+            price: "",
+            imageLocation: "",
+            noOfChapters: ""
+        }
+    }
+
+    state = {
+        // Initially, no file is selected
+        selectedFile: null
+    };
+
+    // On file select (from the pop up)
+    onFileChange = event => {
+        // Update the state
+        this.setState({selectedFile: event.target.files[0]});
+    };
+
+
+    // On file upload (click the upload button)
+    onFileUpload = () => {
+
+        // Create an object of formData
+        const formData = new FormData();
+
+        // Update the formData object
+        formData.append(
             "myFile",
             this.state.selectedFile,
             this.state.selectedFile.name,
-            
-          );
-        
-          // Details of the uploaded file
-          console.log(this.state.selectedFile);
-        
-          // Request made to the backend api
-          // Send formData object
-          axios.post("api/uploadfile", formData);     //update the api path in future
-        };
-        
-        // File content to be displayed after
-        // file upload is complete
-        fileData = () => {
-        
-          if (this.state.selectedFile) {
-             
+        );
+
+        // Details of the uploaded file
+        console.log(this.state.selectedFile);
+
+        // Request made to the backend api
+        // Send formData object
+        axios.post("api/uploadfile", formData);     //update the api path in future
+    };
+
+    // File content to be displayed after
+    // file upload is complete
+    fileData = () => {
+
+        if (this.state.selectedFile) {
             return (
-              <div>
-                <h2>File Details:</h2>
-                 
-    <p>File Name: {this.state.selectedFile.name}</p>
-     
-                 
-    <p>File Type: {this.state.selectedFile.type}</p>
-     
-                 
-    <p>
-                  Last Modified:{" "}
-                  {this.state.selectedFile.lastModifiedDate.toDateString()}
-                </p>
-     
-              </div>
-            );
-          } else {
-            return (
-              <div>
-                <br />
-                <h4>Choose the file before Pressing the Upload button</h4>
-              </div>
-            );
-          }
-        };
-        
-        render() {
-        
-          return (
-              <div>
-            <div className="bookUpload">
-               
-                <h2>
-                 Upload the Ebook here
-                </h2>
                 <div>
-                    <input type="name"  name="email"    //need to add change handler function for name
-                            required="True" placeholder="Book Name" onChange={this.handleChange}></input>
-                    <input type="file" onChange={this.onFileChange} />   
-                    <button onClick={this.onFileUpload}>
-                      Upload!
-                    </button>
+                    <h2>File Details:</h2>
+                    <p>File Name: {this.state.selectedFile.name}</p>
+                    <p>File Type: {this.state.selectedFile.type}</p>
+                    <p>
+                        Last Modified:{" "}
+                        {this.state.selectedFile.lastModifiedDate.toDateString()}
+                    </p>
                 </div>
-              {this.fileData()}
-            </div>
-            </div>
-          );
+            );
+        } else {
+            return (
+                <div>
+                    <br/>
+                    <h4>Choose the file before Pressing the Upload button</h4>
+                </div>
+            );
         }
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="bookUpload">
+                    <h2>
+                        Upload the Ebook here
+                    </h2>
+                    <div>
+                        <form>
+                            <input type="text"
+                                   name="bookName"    //need to add change handler function for name
+                                   required="True"
+                                   placeholder="Book Name"
+                                   value={this.state.bookName}
+                                   onChange={this.handleChange}/>
+                            <input
+                                type="text"
+                                name="isbnNumber"
+                                required="True"
+                                placeholder="ISBN Number"
+                                value={this.state.isbnNumber}
+                                onChange={this.handleChange}/>
+                            <input
+                                type="text"
+                                name="authorName"
+                                required="True"
+                                placeholder="Author name"
+                                value={this.state.authorName}
+                                onChange={this.handleChange}/>
+                            <input
+                                type="text"
+                                name="publisher"
+                                required="True"
+                                placeholder="Publisher"
+                                value={this.state.publisher}
+                                onChange={this.handleChange}/>
+                            <input
+                                type="number"
+                                name="noOfChapters"
+                                required="True"
+                                placeholder="Number of Chapters"
+                                value={this.state.noOfChapters}
+                                onChange={this.handleChange}/>
+                            <input
+                                type="number"
+                                name="price"
+                                required="True"
+                                placeholder="Price"
+                                value={this.state.price}
+                                onChange={this.handleChange}/>
+                            <h3>Upload Book Cover:</h3>
+                            <input type="file" onChange={this.onFileChange}/>
+                            <button onClick={this.onFileUpload}>
+                                Upload!
+                            </button>
+                        </form>
+                    </div>
+                    {this.fileData()}
+                </div>
+                <div className="chapter">
+                    <form>
+                        <input
+                            type="text"
+                            name="chapterName"
+                            required="True"
+                            placeholder="Chapter Name"
+                            //value={this.state.bookName}
+                            onChange={this.handleChange}/>
+                        <input
+                            type="number"
+                            name="chapterPrice"
+                            required="True"
+                            placeholder="Chapter Price"
+                            //value={this.state.chapterPrice}
+                            onChange={this.handleChange}/>
+                        <h4>Upload PDF</h4>
+                        <input type="file" onChange={this.onFileChange}/>
+                        <button onClick={this.onFileUpload}>
+                            Upload!
+                        </button>
+                    </form>
+                </div>
+                <div className="box hide">
+
+                </div>
+            </div>
+        );
     }
+}
+
 export default AdminDashboard
