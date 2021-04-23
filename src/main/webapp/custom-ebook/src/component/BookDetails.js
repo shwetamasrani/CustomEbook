305341f54e4966d6efcb3a48fb1e6261bfd198a9
lumos-ap <ankita008paul@gmail.com>
2021-04-23 10:27:19 +0530

    working on cart

--- src/main/webapp/custom-ebook/src/component/BookDetails.js
+++ src/main/webapp/custom-ebook/src/component/BookDetails.js
@@ -1,54 +1,84 @@
-import React, {Component, useState} from "react"
+import React, {Component} from "react"
 import {Link} from "react-router-dom";
 import productsInfo from "./Data/productInfo";
-import ChaptersInfo from "./Data/ChaptersInfo";
 
 class BookDetails extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            userId:1,
+            bookId: 1,
             chapterData: [],
-            isChecked:true,
-            cartChapters:[]
-            // chapterNames:[],
-            // chapterPrices:[]
+            isChecked: true,
+            cartChapters: [],
+            finalCart: []
         }
-        this.handleChange=this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.addToCart = this.addToCart.bind(this)
+        this.goToCart=this.goToCart.bind(this)
+        this.getBookDetails=this.getBookDetails.bind(this)
+    }
+    async getBookDetails(){
+        let response = await fetch('http://localhost:8081/api/books/'+this.state.bookId, {
+            method: 'GET',
+            headers: {
+                'Accept': '*/*'
+            }
+        })
+        let status = response.status;
+        if (status === 200) {
+            console.log("successful")
+        }
+        return await response.json()
     }
 
     handleChange(event) {
         const {name, value, type, checked} = event.target
-        if(type==="checkbox")
-        {
-            let tempCart=this.state.cartChapters
-            if(checked)
-            {
+        if (type === "checkbox") {
+            let tempCart = this.state.cartChapters
+            if (checked) {
                 tempCart.push(name)
-                console.log("checked",tempCart)
-            }
-            else
-            {
-                tempCart=this.state.cartChapters.filter((chapter) => chapter !== name)
-                console.log("unchecked",tempCart)
+            } else {
+                tempCart = this.state.cartChapters.filter((chapter) => chapter !== name)
 
             }
             this.setState(
-                {[name]: checked, cartChapters:tempCart}
-                )
-        }
-        else
-        {
+                {[name]: checked, cartChapters: tempCart}
+            )
+        } else {
             this.setState({[name]: value})
         }
     }
 
-    async componentDidMount() {
+    addToCart() {
+        let tempCart = [];
+        this.state.cartChapters.forEach(cNum => {
+            tempCart.push({
+                "bookId": this.state.bookId,
+                "chapterNum": cNum
+            })
+        })
+
         this.setState({
-            chapterData: await require('./Data/chapter.json')["chapters"],
+            finalCart: tempCart
+        })
+    }
+
+    goToCart() {
+        this.props.history.push({
+            pathname: "/Cart",
+            cart: this.state.finalCart,
+            userId: this.state.userId
+        })
+    }
+
+    async componentDidMount() {
+        let bookDetails=await this.getBookDetails()
+        this.setState({
+            chapterData: bookDetails["bookChapters"],
             // chapterNames: await require('./Data/chapter.json')["chapters"]["name"],
             // chapterPrices:await require('./Data/chapter.json')["chapters"]["cost"]
-        }, () => console.log(this.state.chapterData))
-
+        })
     }
 
     render() {
@@ -59,11 +89,10 @@ class BookDetails extends Component {
 
                 <div className="BookDetails_Info">
                     <img src={productsInfo[0].img} style={{width: '20rem'}}/>
-                    <div className="Preview">
-                        <Link to={'/Cart'}>
-                            <button>Go to Cart</button>
-                        </Link>
-                    </div>
+                    {this.state.finalCart.length>0 &&(<div className="Preview">
+                        <button onClick={this.goToCart}>Go to Cart</button>
+                    </div>)}
+
                     <p>Name:{bookComponent.bookTitle}</p>
                     <p>Description:{bookComponent.description}</p>
                     <p>Price:{bookComponent.price}</p>
@@ -74,12 +103,12 @@ class BookDetails extends Component {
                             this.state.chapterData.map((chap) => {
                                 return (
                                     <div>
-                                        <li>{chap.name}: ${chap.cost}
-                                        <input type="checkbox"
-                                               name="isChecked"
-                                               onChange={this.handleChange}
-                                               value={this.state.isChecked}
-                                        />
+                                        <li>{chap.chapterName}: ${chap.price}
+                                            <input type="checkbox"
+                                                   name={chap.chapterNumber}
+                                                   onChange={this.handleChange}
+                                                   value={this.state.isChecked}
+                                            />
                                         </li>
                                         <br/>
                                     </div>
@@ -87,7 +116,7 @@ class BookDetails extends Component {
                             })
                         }
                         <br/>
-                        <button className="addToCart"> Add To Cart</button>
+                        <button className="addToCart" onClick={this.addToCart}> Add To Cart</button>
                         <button className="buyNow"> Buy Now</button>
                     </ol>
 
--- src/main/webapp/custom-ebook/src/component/Cart.js
+++ src/main/webapp/custom-ebook/src/component/Cart.js
@@ -6,18 +6,57 @@ class Cart extends Component{
         super(props);
         this.state={
             cart:[],
-            totalSum:0
+            totalSum:0,
+            newCart:this.props.location.cart,
+            userId:this.props.location.userId
         }
         this.removeFromCart=this.removeFromCart.bind(this);
         this.clearCart=this.clearCart.bind(this);
         this.getTotalSum=this.getTotalSum.bind(this);
+        this.getChapterDetails=this.getChapterDetails.bind(this);
+    }
+
+    async getChapterDetails(bookId,chapterNumber){
+        let response = await fetch('http://localhost:8081/api/books/'+bookId, {
+            method: 'GET',
+            headers: {
+                'Accept': '*/*'
+            }
+        })
+        let status = response.status;
+        if (status === 200) {
+            console.log("successful")
+        }
+        let bookDetails=await response.json();
+        let bookName=bookDetails["bookName"]
+        let chapters=bookDetails["bookChapters"]
+        console.log("chapters",chapters)
+        let chapterName,price;
+        for(let i=0;i<chapters.length;i++)
+        {
+            if(chapterNumber===chapters[i]["chapterNumber"])
+            {
+                chapterName=chapters[i]["chapterName"]
+                price=chapters[i]["price"]
+                break;
+            }
+        }
+        let chapterList={
+            bookName:bookName,
+            chapterName:chapterName,
+            price:price
+        }
+        let chapterLists=[bookName,chapterName,price]
+        console.log("chapterList",chapterList)
+        console.log("chaptersList",chapterLists)
+        return chapterList
     }
 
     removeFromCart(chapterToRemove){
         console.log("removed");
-        let tempCart=this.state.cart.filter((chapter) => chapter !== chapterToRemove)
+        let tempCart=this.state.newCart.filter((chapter) => chapter !== chapterToRemove)
         this.setState({
-            cart: tempCart
+            newCart: tempCart
         },()=>this.getTotalSum())
     }
     clearCart()
@@ -27,21 +66,47 @@ class Cart extends Component{
         },()=>this.getTotalSum())
     }
     async componentDidMount() {
-        let tempCart=[]
-        for (let i = 0; i < ChaptersInfo.length; i++) {
-            tempCart.push(ChaptersInfo[i]);
+        // console.log("componentDidMount",this.state.newCart)
+        // console.log("cartinfo1:",localStorage.getItem('oldCart'))
+        // console.log("cartinfo2:",localStorage.getItem('oldCart').length)
+        this.getTotalSum()
+        await this.getChapterDetails(1,5)
+        console.log(this.state.newCart)
+        if(localStorage.getItem('oldCart').length===0)
+        {
+            // console.log("cart length 0")
+            this.setState({
+                cart:this.state.newCart
+            },()=>console.log("callback",this.state.cart))
         }
-        this.setState({
-            cart:tempCart
-        },()=>this.getTotalSum())
+        else
+        {
+            // console.log("cart length >0")
+            let oldCart=JSON.parse(localStorage.getItem('oldCart'))
+            console.log(oldCart)
+            this.state.newCart.forEach(cartItem => {
+                oldCart.push(cartItem)})
+            this.setState({
+                cart:oldCart
+            })
+        }
+        localStorage.setItem('oldCart', JSON.stringify(this.state.cart));
+
+        // let tempCart=[]
+        // for (let i = 0; i < ChaptersInfo.length; i++) {
+        //     tempCart.push(ChaptersInfo[i]);
+        // }
+        // this.setState({
+        //     cart:tempCart
+        // },()=>this.getTotalSum())
     }
 
     getTotalSum (){
-        console.log("inside totalSum:",this.state.cart)
         let sum=0
-        for(let i=0;i<this.state.cart.length;i++)
+        for(let i=0;i<this.state.newCart.length;i++)
         {
-            sum=sum+this.state.cart[i].cost
+            console.log(this.state.newCart[i])
+            sum=sum+this.state.newCart[i].chapterNum
         }
         this.setState({
             totalSum:sum
@@ -51,21 +116,23 @@ class Cart extends Component{
 
 
     render(){
-        let cartItems=this.state.cart.map((chapter, idx) => (
+        let cartItems=this.state.newCart.map((chapter, idx) => {
+            let chapterDetails=this.getChapterDetails(chapter.bookId,chapter.chapterNum)
+            return  (
+
                 <div  key={idx}>
-                    <h3>{chapter.name}</h3>
-                    <h4>${chapter.cost}</h4>
+                    <h3>BookId:{chapter.bookId}</h3>
+                    <h4>Chapter number:{chapter.chapterNum}</h4>
                     <button onClick={() => this.removeFromCart(chapter)}>
                         Remove
                     </button>
                 </div>
-            ))
+            )})
         return (
-
             <div>
                 <h1>Cart</h1>
                 <div className="Chapters">
-                    {this.state.cart.length===0 && (
+                    {this.state.newCart.length===0 && (
                         <h3> Your cart is empty </h3>
                     )}
                     {cartItems}
