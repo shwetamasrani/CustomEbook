a3a380e36bc8b459e55ab32caf1f12d76c9d4fd8
lumos-ap <ankita008paul@gmail.com>
2021-05-15 18:05:23 +0530

    Register Bug fixed

--- src/main/webapp/custom-ebook/src/services/UserService.js
+++ src/main/webapp/custom-ebook/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL="http://localhost:8081/api/user"
+const USER_API_BASE_URL="http://localhost:8081/api/users"
 class UserService{
 
     getUser(user)
